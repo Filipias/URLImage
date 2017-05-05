9ccb0efda3adce12e24fc8324e3afc773990b6fc
te14.elias.grafstrom@umea.nti.se <FaLLaffel9812!>
2017-05-05 09:27:31 +0200

    lol exde

--- src/URLImage.java
+++ src/URLImage.java
@@ -1,14 +1,22 @@
 /**
  * Created by grel14 on 2017-05-05.
+ *
+ * Programm som hämtar alla bilder med taggen "IMG" från hemsidan som användaren väljer
+ *
+ * Måste skrivas http://www.URL.com/underURL.html
+ *
  */
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.*;
 
 public class URLImage {
     public static void main(String[] args) throws Exception {
-        String url = "https://www.oracle.com/index.html";
+        Scanner keyboard = new Scanner(System.in);
+        String UserURL = keyboard.nextLine();
+        String url = UserURL;
         URL oracle = new URL(url);
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(oracle.openStream()));
